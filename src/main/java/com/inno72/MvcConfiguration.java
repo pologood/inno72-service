f968684c8fcfacdf57cd6ecec200a4d22592353d
zb.zhou <qwer1234>
2018-06-27 13:28:59 +0800

    提交

--- src/main/java/com/inno72/MvcConfiguration.java
+++ src/main/java/com/inno72/MvcConfiguration.java
@@ -20,7 +20,6 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.inno72.common.interceptor.LogInterceptor;
 import com.inno72.common.interceptor.PageListAttrHandlerInterceptor;
 import com.inno72.common.spring.JsonView;
 import com.inno72.redis.IRedisUtil;
@@ -87,9 +86,9 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new PageListAttrHandlerInterceptor()).addPathPatterns("/**");
-        LogInterceptor logInterceptor = new LogInterceptor();
-        logInterceptor.setRedisUtil(redisUtil);
-        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+//        LogInterceptor logInterceptor = new LogInterceptor();
+//        logInterceptor.setRedisUtil(redisUtil);
+//        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
     }
 
 }
--- src/main/java/com/inno72/service/UserInfoService.java
+++ src/main/java/com/inno72/service/UserInfoService.java
@@ -1,7 +1,5 @@
 package com.inno72.service;
 
-import java.util.List;
-
 import com.inno72.common.Service;
 import com.inno72.model.UserInfo;
 
